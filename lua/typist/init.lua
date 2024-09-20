642ce0e77f88484d44b42127fbbec0a129490f80
Nitsan Avni <nitsanav@gmail.com>
2024-09-20 11:11:19 +0200

    ! B nvim user cmd name

--- lua/typist/init.lua
+++ lua/typist/init.lua
@@ -5,7 +5,7 @@ M.expand_file_refs_in_current_buf = function()
 end
 
 M.setup = function()
-	vim.api.nvim_create_user_command("Typist.expand_file_refs_in_current_buf", M.expand_file_refs_in_current_buf, {})
+	vim.api.nvim_create_user_command("TypistExpand", M.expand_file_refs_in_current_buf, {})
 end
 
 return M
