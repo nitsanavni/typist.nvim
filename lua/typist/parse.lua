9150ccc0c924c3c4f51488e25cd8fb4f67b430ee
Nitsan Avni <nitsanav@gmail.com>
2024-09-21 09:51:28 +0200

    ! F add path to parsing

--- lua/typist/parse.lua
+++ lua/typist/parse.lua
@@ -1,9 +1,14 @@
-local function parse_response(response)
+local filepath_utils = require("filepath_utils")
+
+local function parse_response(response, search_paths)
+	search_paths = search_paths or {}
 	local files = {}
 	for file_name, content in response:gmatch("## File: `([^`]+)`%s*```%w*%s*([%s%S]-)%s*```") do
+		local resolved_path = filepath_utils.resolve_filepath(file_name, search_paths)
 		table.insert(files, {
 			name = file_name,
 			content = content,
+			path = resolved_path,
 		})
 	end
 	return files
